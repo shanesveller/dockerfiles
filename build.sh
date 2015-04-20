5217dee94371c79171fbca002a6d6f882591072e
Shane Sveller <shanesveller@gmail.com>
2015-04-20 11:41:34 -0500

    Build script update

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ NS=shanesveller
 
 docker build -t $NS/elixir elixir/
 docker build -t $NS/phoenix phoenix/
+docker build -t $NS/mumble mumble/
+docker build -t $NS/teamspeak teamspeak/
+docker build -t $NS/plug-test plug-test/
