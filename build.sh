8d70bbf598762a51559eeaa30f81807878ff74de
Shane Sveller <shanesveller@gmail.com>
2015-06-05 16:34:00 -0500

    Use remote images

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ NS=shanesveller
 
 # grep 'FROM' **/Dockerfile | awk '!/shanesveller/ {print $2}' | sort | uniq | xargs -n1 docker pull
 
-docker build -t $NS/elixir-lang elixir/
-docker build -t $NS/phoenix phoenix/
+docker pull shanesveller/elixir-lang
+docker pull shanesveller/phoenix
 docker build -t $NS/mumble mumble/
 docker build -t $NS/teamspeak teamspeak/
 docker build -t $NS/steamcmd steamcmd/
